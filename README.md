41e44bbdcfdf2308bae4f48e357fc748fcda1543
Kyle Partridge <kyle.partridge@sendgrid.com>
2018-01-17 11:45:53 -0800

    Initial round of app returning a visitor count with some basic error handling

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # go-visit
+
 A simple visitor count tracking HTTP app build in golang
+
+## Development
+
+This project uses [dep](https://github.com/golang/dep) for dependency management.
--- main.go
+++ main.go
@@ -1,5 +1,82 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
 
+	redis "gopkg.in/redis.v3"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	Host string
+	Port int
+
+	RedisKey string
+
+	RedisADDR     string
+	RedisPassword string
+	RedisDB       int64 `envconfig`
+}
+
+const APP_NAME = "visit"
+
+type App struct {
+	redis  *redis.Client
+	config *Config
+}
+
+func (a *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	cmd := a.redis.Incr(a.config.RedisKey)
+	if cmd.Err() != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "Error contacting redis: %s\n", cmd.Err())
+		return
+	}
+
+	count, err := cmd.Result()
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "Error contacting redis: %s\n", cmd.Err())
+		return
+	}
+
+	fmt.Fprintf(w, "The current visit count is %d.\n", count)
+}
+
+func main() {
+	var config Config
+	err := envconfig.Process(APP_NAME, &config)
+	if err != nil {
+		log.Fatalf("error reading config: %s", err)
+	}
+
+	log.Printf("starting with config: %+v", config)
+
+	addr := fmt.Sprintf("%s:%d", config.Host, config.Port)
+	log.Printf("listening on %q", addr)
+
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("error listening on %q: %s", addr, err)
+	}
+	defer listener.Close()
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     config.RedisADDR,
+		Password: config.RedisPassword,
+		DB:       config.RedisDB,
+	})
+
+	app := App{
+		redis:  client,
+		config: &config,
+	}
+
+	if err := http.Serve(listener, &app); err != nil {
+		log.Fatalf("error serving http: %s", err)
+	}
 }
